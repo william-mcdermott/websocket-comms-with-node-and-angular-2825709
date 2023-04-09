75e046f13f38d6e0c463d50f913b1c800e38ae32
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-04-09 11:46:42 -0500

    Display messages in mat-cards and add mat-input to post new message

--- packages/webapp/src/app/app.module.ts
+++ packages/webapp/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MatCardModule, MatInputModule],
   providers: [],
   bootstrap: [AppComponent],
 })
