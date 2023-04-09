50d54183d1ec9a8a385d0303414c3c02edc4d2ad
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-04-09 11:34:15 -0500

    Initial commit of web interface

--- packages/webapp/src/app/app.component.ts
+++ packages/webapp/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatRelayMessage, User } from '@websocket/types'
 
 @Component({
   selector: 'websocket-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'I am Angular';
+  public messages: ChatRelayMessage[];
+  public currentUser: User;
+
+  ngOnInit() {
+    this.messages = [
+      {event: 'chatRelay', author: {name: 'Jane', id: 1}, contents: 'Hi this is Jane'},
+      {event: 'chatRelay', author: {name: 'Henry', id: 2}, contents: 'Hello Jane I\'m Henry'},
+    ]
+
+    this.currentUser = { name: 'Xavier', id: 3 };
+  }
 }
